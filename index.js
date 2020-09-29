98ced68c6e52f6fd63924e0986d71e51926d3c29
Jay <jvelayud@gmail.com>
2020-09-29 15:38:29 -0400

    Adding proper path

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   testImport: function() {
-    return fs.readFileSync("./TEST", "utf8");
+    return fs.readFileSync(path.join(__dirname, "TEST"), "utf8");
   }
 };
